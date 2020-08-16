9c1d482bcf8e6a3ea910527f35f2fa764f06cad0
Kiermasz <raczynski90@gmail.com>
2020-08-16 14:37:39 +0200

    initial logic for weight changes all tests passed

---
+++ LiftoffApp/static/logic.py
@@ -0,0 +1,20 @@
+def is_sets_valid(sets):
+    sets_over_five = [set for set in sets if set > 5]
+    sets_negative = [set for set in sets if set < 0]
+    if sets_over_five or sets_negative:
+        raise ValueError(f"Invalid number of reps in a set {[set for set in sets if set > 5]}")
+
+
+def is_passed(sets: list) -> bool:
+    if sum(sets) < 25:
+        return False
+    return True
+
+
+def calculate_new_weights(lifts: dict) -> dict:
+    for key, value in lifts.items():
+        sets = value[-1]
+        previous_weight = float(value[0])
+        if is_passed(sets):
+            lifts[key] = (previous_weight + 2.5, [0, 0, 0, 0, 0])
+    return lifts
--- LiftoffApp/tests.py
+++ LiftoffApp/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+import unittest
+from LiftoffApp.static.logic import is_passed, calculate_new_weights, is_sets_valid
 
-# Create your tests here.
+
+class TestLogic(unittest.TestCase):
+    def setUp(self):
+        self.test_data = {'cable butterflies': (15, [0, 0, 0, 0, 0]),
+                          'benchpress': (90, [5, 4, 4, 4, 4]),
+                          'row': (60, [5, 5, 5, 5, 5]),
+                          'bicep curl': (12, [5, 5, 5, 5, 5])}
+
+    def test_is_passed_when_sets_completed(self):
+        test_expression = [5, 5, 5, 5, 5]
+        self.assertTrue(is_passed(test_expression))
+
+    def test_is_passed_when_sets_not_completed(self):
+        test_expression = [5, 5, 5, 4, 4]
+        self.assertFalse(is_passed(test_expression))
+
+    def test_sets_validation_if_too_many_reps(self):
+        test_expression = [6, 10, 5, 4, 4]
+        self.assertRaises(ValueError, is_sets_valid, test_expression)
+
+    def test_sets_validation_if_negative_reps(self):
+        test_expression = [6, -1, 5, 4, 4]
+        self.assertRaises(ValueError, is_sets_valid, test_expression)
+
+    def test_new_weights_calculation(self):
+        expected = {'cable butterflies': (15, [0, 0, 0, 0, 0]),
+                    'benchpress': (90, [5, 4, 4, 4, 4]),
+                    'row': (62.5, [0, 0, 0, 0, 0]),
+                    'bicep curl': (14.5, [0, 0, 0, 0, 0])}
+        actual = calculate_new_weights(self.test_data)
+        self.assertEqual(expected, actual)
